11cb9b1ba12f380ec03631a3619f2fe057a274e6
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2026-06-21 00:27:00 +0530

    Add pipeline using Predicate Function and BinaryOperator

--- src/main/java/com/saksham/Main.java
+++ src/main/java/com/saksham/Main.java
@@ -2,16 +2,22 @@ package com.saksham;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Arrays;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers =
+                Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        Integer miniResult =
+                new MiniStream<>(numbers)
+                        .filter(n -> n % 2 == 0)
+                        .map(n -> n * n)
+                        .reduce(0, Integer::sum);
+
+        System.out.println("MiniStream Result = " + miniResult);
     }
 }
